6db0585b584aa3ae98b593fb068cb1be6fcd3901
onoja64 <corneliussamuel64@gmail.com>
2021-08-30 20:09:36 +0100

    commit1

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,27 @@
+import { MenuEntry } from '@onoja64/boomui'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/'
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap'
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool'
+      }
+    ]
+  },
+  
+]
+
+export default config
